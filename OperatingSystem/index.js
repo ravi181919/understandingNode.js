36bd4309500aad4fbb1daf59cea19e114ada57f0
ravigupta <rg187888@gmail.com>
2024-05-08 17:20:34 +0530

    some changes in os module

--- OperatingSystem/index.js
+++ OperatingSystem/index.js
@@ -1,12 +1,21 @@
 const os = require('os');
 
-console.log(os.arch());
-console.log(os.freemem());
-console.log(os.totalmem());
-console.log(JSON.stringify(os.networkInterfaces()));
-console.log(os.tmpdir());
-console.log(os.endianness());
-console.log(os.hostname());
-console.log(os.type());
-console.log(os.platform());
-console.log(os.release());
+console.log('OS Architecture ' + os.arch());
+
+console.log('free memory space ' + os.freemem());
+
+console.log('Total memory space ' + os.totalmem());
+
+console.log('OS net work Interface ' + JSON.stringify(os.networkInterfaces()));
+
+console.log('Os default temp dir ' + os.tmpdir());
+
+console.log('order of bits in a sequence within a binary representation of a number' + os.endianness());
+
+console.log('OS Hostname ' + os.hostname());
+
+console.log('OS type ' + os.type());
+
+console.log('OS platform ' + os.platform());
+
+console.log('OS release date ' + os.release());

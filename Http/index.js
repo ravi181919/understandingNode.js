9d8b2a23789a8bf8823fa2244d1ebde03559b79a
ravigupta <rg187888@gmail.com>
2024-05-08 18:20:25 +0530

    some upDate in http server module

--- Http/index.js
+++ Http/index.js
@@ -1,10 +1,16 @@
-const http = require('http');
+const http = require("http");
 
-const server =  http.createServer((req, res) => {
-    if(req.url == '/'){
-        res.write('<h1>Hello Ravi how are you today? </h1>')
-    }
-    res.end();
-})
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("<h1>Hello Ravi how are you today? </h1>");
+  } else if (req.url == "/About") {
+    res.write("<h1>Ravi! now you are on About page. </h1>");
+  } else if (req.url == "/Skill") {
+    res.write("<h1>Ravi! now you are on Skill page. </h1>");
+  } else if (req.url == "/Contact") {
+    res.write("<h1>Ravi! now you are on Contact page. </h1>");
+  }
+  res.end();
+});
 
-server.listen(4000)
+server.listen(4003);

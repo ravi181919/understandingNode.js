6261ab9719bfabde068b51df0e98787826a1b89f
ravigupta <rg187888@gmail.com>
2024-05-09 10:27:45 +0530

    in httpserver module statusCode 500 check

--- HttpServer/index.js
+++ HttpServer/index.js
@@ -1,5 +1,4 @@
 const http = require("http"); // first method of import
-
 // import http from 'http'    // second method of import
 
 const PORT = 1819;
@@ -12,14 +11,26 @@ const server = http.createServer((req, res) => {
 //   console.log(`Server is running at ${HOSTNAME}: ${PORT} `);
 // });
 
-// now try =>  setHeader() and statusCode
+/*       now try =>  setHeader() and statusCode  [information =>  https://www.google.com/]  */
 
+// try 200 statusCode =>
 const newPort = 1919;
 const newServer = http.createServer((req, res) => {
-  res.statusCode = 200; //  information =>  https://www.google.com/
-  res.setHeader("Content-Type", "text/plain"); // information =>  https://www.google.com/
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
   res.end("node is working!");
 });
-newServer.listen(newPort, () => {
-    console.log(`Server is running at ${HOSTNAME}: ${newPort} `);
-  });
+// newServer.listen(newPort, () => {
+//   console.log(`Server is running at ${HOSTNAME}: ${newPort} `);
+// });
+
+// try 500 statusCode =>
+const changePort = 2020;
+const changeServer = http.createServer((req, res) => {
+  res.statusCode = 500;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ error: "server-error" }));
+});
+changeServer.listen(changePort, () => {
+  console.log(`Server is running at ${HOSTNAME}: ${changePort} `);
+});

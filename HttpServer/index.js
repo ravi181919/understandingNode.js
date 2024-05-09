ce56aa61348dd1b45effa69f5cb86dbcf962817a
ravigupta <rg187888@gmail.com>
2024-05-09 10:14:54 +0530

    in httpserver module add setHeader and statusCode 200

--- HttpServer/index.js
+++ HttpServer/index.js
@@ -1,13 +1,25 @@
-const http = require('http'); // first method of import 
+const http = require("http"); // first method of import
 
-// import http from 'http'    // second method of import 
+// import http from 'http'    // second method of import
 
 const PORT = 1819;
-const HOSTNAME = 'localhost';
+const HOSTNAME = "localhost";
 
 const server = http.createServer((req, res) => {
-    res.end('Node is working!');
+  res.end("Node is working!");
 });
-server.listen(PORT, () => {
-    console.log(`Server is running at ${HOSTNAME}: ${PORT} `);
-})
+// server.listen(PORT, () => {
+//   console.log(`Server is running at ${HOSTNAME}: ${PORT} `);
+// });
+
+// now try =>  setHeader() and statusCode
+
+const newPort = 1919;
+const newServer = http.createServer((req, res) => {
+  res.statusCode = 200; //  information =>  https://www.google.com/
+  res.setHeader("Content-Type", "text/plain"); // information =>  https://www.google.com/
+  res.end("node is working!");
+});
+newServer.listen(newPort, () => {
+    console.log(`Server is running at ${HOSTNAME}: ${newPort} `);
+  });
